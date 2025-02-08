5847c6f288d92d1546c28edc46f0482c97482882
Sameur Rouag <smrrawek@gmail.com>
2025-02-08 19:56:57 +0100

    Adding MusicLoad Function

---
+++ script.js
@@ -0,0 +1,57 @@
+allMusic = [
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img:"music-1",
+        src:"music-1"
+    },
+    {
+        name: "Ikson AnyWhere - Ikson",
+        artist: "Audio Library",
+        img:"music-2",
+        src:"music-2"
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img:"music-3",
+        src:"music-3"
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img:"music-4",
+        src:"music-4"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img:"music-5",
+        src:"music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img:"music-6",
+        src:"music-6"
+    }
+];
+
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+mainAudio = wrapper.querySelector("#main-audio");
+
+let musicIndex = 4;
+
+window.addEventListener("load", ()=>{
+    loadMusic(musicIndex);
+});
+
+function loadMusic(indexNumb){
+    musicName.innerText = allMusic[indexNumb-1].name;
+    musicArtist.innerText = allMusic[indexNumb-1].artist;
+    musicImg.src = `images/${allMusic[indexNumb-1].img}.jpg`;
+    mainAudio.src = `songs/${allMusic[indexNumb-1].src}.mp3`;
+}
